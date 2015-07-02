7a7f97fbae03efb4834558f7b94d197d59c953c8
Anindita Basak <anindita.basak@live.com>
2015-07-02 15:41:36 +0530

    Sending realtime Tweets to Azure Service Bus EventHub & Windows Phone Sensor Data to Event Hub
    Microsoft IoT Foundation :+1:

---
+++ SendTweetsToEventHub/Tweets.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendTweetsToEventHub
+{
+    class Tweets
+    {
+        public string DateTime { get; set; }
+
+        public string TwitterUserName { get; set; }
+
+        public string ProfileLocation { get; set; }
+
+       public string MorePreciseLocation { get; set; }
+
+        public string Country { get; set; }
+
+        public string TweetID { get; set; }
+
+       
+    }
+}
